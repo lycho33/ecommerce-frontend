629904ca5c2e80309a48cf2e06411f450da124d2
lycho33 <cho.lyds@gmail.com>
2021-12-19 15:21:03 -0500

    refactored PrdouctIndex into hooks

--- src/containers/ProductIndex.js
+++ src/containers/ProductIndex.js
@@ -1,88 +1,17 @@
-// import { useEffect, useState } from "react"
-// import {connect} from 'react-redux'
-
-// import {getProducts} from '../redux/actions/product'
-
-// import ProductItem from '../components/ProductItem'
-// import SearchProducts from '../components/SearchProducts'
-// import CartContainer from './CartContainer'
-// import { render } from "react-dom"
-
-// const PAGE_PRODUCTS = 'products';
-// const PAGE_CART = 'cart'
-
-// function ProductIndex({getProducts, products}) {
-
-//     // filterProdcuts(name) {
-//     //     this.props.products.filter((product) => product.name == name )
-//     // }
-
-//     //STATE/////////////////////////////////////////////////////////////////////////////////////////////////
-    // const [cartItems, setCartItems] = useState([]);
-//     const [page, setPage] = useState(PAGE_PRODUCTS)
-
-//     //FUNCTIONS///////////////////////////////////////////////////////////////////////////////////////////////////////
-//     const addToCart = (product) => {
-//         // console.log("add to cart")
-//         setCartItems([...cartItems, {...product}])
-//         // console.log(cartItems)
-//     }
-//     const renderProducts = () => (
-//         products.map((products) => <ProductItem {...products} products={products} key={products.id} addToCart={addToCart} /> )
-//     )
-//     const renderCart = () => {
-//         return <CartContainer navigateTo={navigateTo} cartItems={cartItems} pageCart={PAGE_CART} removeItem={removeFromCart}/>
-//     }
-//     const navigateTo = (nextPage) => {
-//         setPage(nextPage)
-//         console.log(nextPage)
-//     }
-//     const removeFromCart = (productRemoved) => {
-//         setCartItems(cartItems.filter(product => product !== productRemoved))
-//     }
-//     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//     useEffect(getProducts, [getProducts])
-
-//         return (
-//             <div>
-//                 <button onClick={() => navigateTo(PAGE_CART)}>Go To Cart ({cartItems.length})</button>
-//                 <button onClick={() => navigateTo(PAGE_PRODUCTS)}>View Products</button>
-//                 {page === PAGE_CART && renderCart()}
-//                 {page === PAGE_PRODUCTS && renderProducts()}
-//             </div>
-//         )
-//     }
-
-
-// const mapStateToProps = (state) => {
-//     return {products: state.products}
-// }
-
-// export default connect(mapStateToProps, {getProducts})(ProductIndex)
-
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import ProductItem from '../components/ProductItem'
-import SearchProducts from '../components/SearchProducts'
-import CartContainer from './CartContainer'
-import { render } from "react-dom"
-import {getProducts} from '../redux/actions/product'
 
-class ProductIndex extends Component {
+function ProductIndex({products, cart}) {
+    const renderProducts = () => (
+        products.map((products) => <ProductItem {...products} products={products} key={products.id} cart={cart}/> )
+    )
 
-
-    render() {
-        const renderProducts = () => (
-            this.props.products.map((products) => <ProductItem {...products} products={products} key={products.id}  addToCart={this.props.addToCart} cart={this.props.cart}/> )
-        )
-
-        return (
-            <div>
-                {renderProducts()}
-            </div>
-        )
-    }
+    return (
+        <div>
+             {renderProducts()}
+        </div>
+    )
 }
 
 const mapStateToProps = (state) => {
