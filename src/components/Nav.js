1aa5ceca63510d72dfc7f8d8598733a01d9542b5
lycho33 <cho.lyds@gmail.com>
2022-03-18 17:42:33 -0400

    no changes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import './css/Nav.css'
 import './css/Home.css'
 import * as FaIcons from "react-icons/fa"
@@ -7,12 +8,10 @@ import * as AiIcons from "react-icons/ai"
 import {SidebarData} from './SidebarData'
 import NavLinks from './NavLinks'
 
+
 function Nav() {
     const [sidebar, setSidebar] = useState(false)                              
-    const showSideBar = () => {
-        setSidebar(!sidebar)
-        console.log("after,", sidebar)
-    }
+    const showSideBar = () => { setSidebar(!sidebar) }
     const [pathname, setPathname] = useState('')
 
     return (
@@ -22,7 +21,6 @@ function Nav() {
                 <FaIcons.FaBars onClick={showSideBar}/>
             </Link>
             <h1 className='pathname'>{(pathname) ? `${pathname}` : 'The Mini-Store'}</h1>
-            
         </div>
         <nav className={!sidebar ? 'nav-menu active' : 'nav-menu'}>
             <ul className="nav-menu-items" onClick={showSideBar}>
